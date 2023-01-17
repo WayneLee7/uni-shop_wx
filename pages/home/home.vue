97e825531d9d970a9cfd1476c77da51e3870d4cc
WayneLee7 <waynelee735@hotmail.com>
2023-01-17 17:08:45 +0800

    完成首页

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,40 @@
 <template>
 	<view>
-		Home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular>
+			<swiper-item v-for="item in swiperList" :key="item.goods_id">
+				<navigator :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id" class="swiper-item">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+
+		<!-- 分类导航 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item,index) in navList" :key="index" @click="navClickHander(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+		<!-- 楼层 -->
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,i) in floorList" :key="i">
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<navigator :url="item.product_list[0].url" class="left-img-box">
+						<image :src="item.product_list[0].image_src"
+							:style="{width:item.product_list[0].image_width+'rpx'}" mode="widthFix"></image>
+					</navigator>
+					<view class="right-img-box">
+						<navigator :url="item2.url" class="right-img-item" v-for="(item2,i2) in item.product_list" :key="i2">
+							<image :src="item2.image_src" mode="widthFix" v-if="i2!==0"
+								:style="{width:item2.image_width+'rpx'}"></image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+
+		</view>
+
 	</view>
 </template>
 
@@ -8,12 +42,90 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [],
+				navList: [],
+				floorList: []
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+			this.getfloorList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.swiperList = res.message
+			},
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+			},
+			navClickHander(item) {
+				if (item.name === '分类') {
+					// console.log("2");
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			},
+			async getfloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				res.message.forEach(floor=>{
+					floor.product_list.forEach(prod=>{
+						prod.url='/subpkg/goods_list/goods_list?'+prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+
+	}
+
+	.nav-list {
+		display: flex;
+		margin: 15px 0;
+		justify-content: space-around;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-title {
+		width: 100%;
+		height: 60rpx;
+	}
+
+	.floor-img-box {
+		display: flex;
+		padding-left: 10rpx;
+		.right-img-box {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+		}
+	}
 </style>
